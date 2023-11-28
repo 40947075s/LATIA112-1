f4e486e74f93fde22fe2c0917946d1b3aaf0f0d5
40947075s <40947075s@gapps.ntnu.edu.tw>
2023-11-28 15:05:02 +0800

    complete data preprocess

--- hello_plotly/main.js
+++ hello_plotly/main.js
@@ -1,79 +1,98 @@
-d3.csv(
-  "https://raw.githubusercontent.com/ryanchung403/dataset/main/harry_potter.csv",
-).then((res) => {
-  console.log("Local csv:", res);
-  drawLineChart(res);
-});
+function preprocessCsvData(data) {
+  data.forEach((row) => {
+    // Replace all '年' with ''
+    if (row["學年度別"]) {
+      row["學年度別"] = row["學年度別"].replace(/年/g, "");
+    }
 
-function drawLineChart(res) {
-  let myGraph = document.getElementById("myGraph");
+    // Replace all '-' with 0
+    for (const key in row) {
+      if (Object.prototype.hasOwnProperty.call(row, key)) {
+        row[key] = row[key].replace(/-/g, "0");
+      }
+    }
+  });
 
-  let trace1 = {};
-  trace1.mode = "lines+markers";
-  trace1.type = "scatter";
-  trace1.name = "Revenue";
-  trace1.marker = {
-    size: 10,
-  };
-  trace1.x = [];
-  trace1.y = [];
-  trace1.text = [];
-  trace1.textposition = "bottom center";
-  trace1.textfont = {
-    family: "Raleway, sans-serif",
-    size: 15,
-    color: "blue",
-  };
-
-  for (let i = 0; i < res.length; i++) {
-    trace1.x[i] = res[i]["release_year"];
-    trace1.y[i] = res[i]["revenue"];
-    trace1.text[i] = res[i]["title"];
-  }
-
-  let trace2 = {};
-  trace2.mode = "lines+markers";
-  trace2.type = "scatter";
-  trace2.name = "Budget";
-  trace2.marker = {
-    size: 10,
-  };
-  trace2.x = [];
-  trace2.y = [];
-  trace2.text = [];
-  trace2.textposition = "bottom center";
-  trace2.textfont = {
-    family: "Raleway, sans-serif",
-    size: 15,
-    color: "blue",
-  };
-
-  for (let i = 0; i < res.length; i++) {
-    trace2.x[i] = res[i]["release_year"];
-    trace2.y[i] = res[i]["budget"];
-    trace2.text[i] = res[i]["title"];
-  }
-
-  let data = [];
-  data.push(trace1);
-  data.push(trace2);
-
-  let layout = {
-    margin: {
-      t: 80,
-    },
-    title: "Harry Potter 2001 ~ 2011",
-    // xaxis: {
-    //     range: [0, 6]
-    // },
-    // yaxis: {
-    //     range: [0, 25]
-    // }
-  };
-
-  Plotly.newPlot(myGraph, data, layout);
+  return data;
 }
-let myGraph = document.getElementById("myGraph");
+
+d3.csv("Graduate_Outcomes_of_Taipei_City_Secondary_Schools.csv")
+  .then((data) => {
+    data = preprocessCsvData(data);
+
+    console.log(data);
+  })
+  .catch((error) => console.error("Error fetching or parsing CSV:", error));
+
+// function drawLineChart(res) {
+//   let myGraph = document.getElementById("myGraph");
+
+//   let trace1 = {};
+//   trace1.mode = "lines+markers";
+//   trace1.type = "scatter";
+//   trace1.name = "Revenue";
+//   trace1.marker = {
+//     size: 10,
+//   };
+//   trace1.x = [];
+//   trace1.y = [];
+//   trace1.text = [];
+//   trace1.textposition = "bottom center";
+//   trace1.textfont = {
+//     family: "Raleway, sans-serif",
+//     size: 15,
+//     color: "blue",
+//   };
+
+//   for (let i = 0; i < res.length; i++) {
+//     trace1.x[i] = res[i]["release_year"];
+//     trace1.y[i] = res[i]["revenue"];
+//     trace1.text[i] = res[i]["title"];
+//   }
+
+//   let trace2 = {};
+//   trace2.mode = "lines+markers";
+//   trace2.type = "scatter";
+//   trace2.name = "Budget";
+//   trace2.marker = {
+//     size: 10,
+//   };
+//   trace2.x = [];
+//   trace2.y = [];
+//   trace2.text = [];
+//   trace2.textposition = "bottom center";
+//   trace2.textfont = {
+//     family: "Raleway, sans-serif",
+//     size: 15,
+//     color: "blue",
+//   };
+
+//   for (let i = 0; i < res.length; i++) {
+//     trace2.x[i] = res[i]["release_year"];
+//     trace2.y[i] = res[i]["budget"];
+//     trace2.text[i] = res[i]["title"];
+//   }
+
+//   let data = [];
+//   data.push(trace1);
+//   data.push(trace2);
+
+//   let layout = {
+//     margin: {
+//       t: 80,
+//     },
+//     title: "Harry Potter 2001 ~ 2011",
+//     // xaxis: {
+//     //     range: [0, 6]
+//     // },
+//     // yaxis: {
+//     //     range: [0, 25]
+//     // }
+//   };
+
+//   Plotly.newPlot(myGraph, data, layout);
+// }
+// let myGraph = document.getElementById("myGraph");
 
 // let trace1 = {};
 // trace1.mode = "markers";
